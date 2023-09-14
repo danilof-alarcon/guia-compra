050b2f64d075bc0aacf23ce72cfa672d9265754d
DANILOALARCON <danilof.alarcon@gmail.com>
2023-09-13 20:26:55 -0500

    Importing data from Airtable to project and displaying it on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
+import getCameras from './lib/airtable';
 import './App.css'
 
 function App() {
-  return (
-    <h1>Hello, world!</h1>
-  )
+
+    const [cameras, setCameras] = useState([]);
+
+    useEffect(() => {
+        getCameras()
+        .then((response) => {
+            setCameras(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        }); 
+    }, []);
+
+    return (
+        <div>
+            <h1>Cameras</h1>
+            <ul>
+                {cameras.map((camera) => {
+                    return (
+                        <li key={camera.id}>
+                            <h3>{camera.fields.Marca} {camera.fields.NombreCamara}</h3>
+                            <img src={camera.fields.Foto[0].url} alt={camera.fields.NombreCamara} />
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default App
