2762747aa7d0300d87ea886fabbbd51857baa644
DANILOALARCON <danilof.alarcon@gmail.com>
2023-09-15 17:59:25 -0500

    Development

---
+++ src/styles/custom-styles.js
@@ -0,0 +1,14 @@
+const styles = {
+    components: {
+        button: { 
+            paddingY: 1, 
+            borderRadius: 2,
+            boxShadow: "none",
+            '&:hover': {
+                boxShadow: "none",
+            }
+        }
+    },
+}
+
+export default styles
