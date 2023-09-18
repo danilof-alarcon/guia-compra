f9a3d8768a499b2893e2a956ace383ee2757a271
DANILOALARCON <danilof.alarcon@gmail.com>
2023-09-18 11:07:46 -0500

    Edit

--- .cors.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    origins: ['*'],
-    credentials: true,
-};
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
-const { corsPlugin } = require('@vitejs/plugin-cors');
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), corsPlugin({
-    credentials: false,
-  })],
+  plugins: [react()],
   server: {
     host: true,
-  },
+  }
 })
