20d90d4b867bfc9ef54ea513d191e005898cfd5a
DANILOALARCON <danilof.alarcon@gmail.com>
2023-09-18 10:51:15 -0500

    Cors correction

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), corsPlugin({
+    credentials: false,
+  })],
   server: {
     host: true,
   },
-  plugins: [corsPlugin({
-    credentials: false,
-  })],
 })
